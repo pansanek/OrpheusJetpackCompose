53b377bb1e9a4986ba8d2cd0fe30b2728320680c
SashaPotemkin <alex.potjomkin@gmail.com>
2023-09-21 14:22:14 +0300

    Gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0-alpha04")
 }
