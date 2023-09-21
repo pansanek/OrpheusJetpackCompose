2ac1e5ca20ad42b8ee1c0fd6493a16667ca8af3a
SashaPotemkin <alex.potjomkin@gmail.com>
2023-09-21 14:20:00 +0300

    UI Theme

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/theme/Color.kt
@@ -0,0 +1,10 @@
+package ru.potemkin.orpheusjetpackcompose.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Black = Color(0xFF1F2421)
+val Green = Color(0xFF49A078)
+val LightGreen = Color(0xFF9CC5A1)
+val Turquoise = Color(0xFF216869)
+val White = Color(0xFFDCE1DE)
--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/theme/Theme.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/theme/Theme.kt
@@ -1,8 +1,10 @@
-package ru.potemkin.orpheusjetpackcompose.presentation
+package ru.potemkin.orpheusjetpackcompose.presentation.theme
 
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -10,33 +12,27 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import ru.potemkin.orpheusjetpackcompose.presentation.components.ButtonComponent
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Black,
+    secondary = Turquoise,
+    tertiary = Green
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = White,
+    secondary = Turquoise,
+    tertiary = Green
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
-
 @Composable
 fun OrpheusJetpackComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
