8b88e8aac947cf41fa3ffa356d6a32bc861f42c2
SashaPotemkin <alex.potjomkin@gmail.com>
2023-09-21 14:19:36 +0300

    UI Components

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/components/AuthButton.kt
@@ -0,0 +1,69 @@
+package ru.potemkin.orpheusjetpackcompose.presentation.components
+
+import ru.potemkin.orpheusjetpackcompose.presentation.theme.Green
+import ru.potemkin.orpheusjetpackcompose.presentation.theme.LightGreen
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun AuthButton(
+    text: String,
+    backgroundColor: Color,
+    contentColor: Color,
+    enabled: Boolean = true,
+    onButtonClick: () -> Unit,
+    isLoading: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        modifier = modifier,
+        onClick = {
+            onButtonClick()
+        },
+        shape = RoundedCornerShape(25.dp),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = contentColor,
+            disabledContentColor = contentColor,
+            containerColor = backgroundColor,
+            disabledContainerColor = backgroundColor
+        ),
+        enabled = enabled
+    ) {
+        if(isLoading) {
+            CircularProgressIndicator(
+                color = contentColor,
+                modifier = Modifier.size(20.dp)
+            )
+        } else {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AuthButtonPreview() {
+    AuthButton(
+        text = "Войти",
+        backgroundColor = Green,
+        contentColor = LightGreen,
+        onButtonClick = { /*TODO*/ },
+        isLoading = false,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/components/TextEntryModule.kt
@@ -0,0 +1,121 @@
+package ru.potemkin.orpheusjetpackcompose.presentation.components
+
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.RemoveRedEye
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.potemkin.orpheusjetpackcompose.presentation.theme.Black
+import ru.potemkin.orpheusjetpackcompose.presentation.theme.Green
+import ru.potemkin.orpheusjetpackcompose.presentation.theme.LightGreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextEntryModule(
+    description: String,
+    modifier: Modifier = Modifier,
+    hint: String,
+    leadingIcon: ImageVector,
+    textValue: String,
+    keyboardType: KeyboardType = KeyboardType.Ascii,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    textColor: Color,
+    cursorColor: Color,
+    onValueChanged: (String) -> Unit,
+    trailingIcon: ImageVector? = null,
+    onTrailingIconClick: (() -> Unit)?
+) {
+    Column(
+        modifier = modifier
+    ) {
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 3.dp)
+                .height(60.dp),
+            label = { Text(text = description, color = LightGreen) },
+            value = textValue,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.White,
+                cursorColor = cursorColor,
+                focusedIndicatorColor = Color.Green,
+                unfocusedIndicatorColor = LightGreen
+            ),
+            onValueChange = onValueChanged,
+            shape = RoundedCornerShape(25.dp),
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = leadingIcon,
+                    contentDescription = null,
+                    tint = cursorColor
+                )
+            },
+            trailingIcon = {
+                if(trailingIcon != null){
+                    Icon(
+                        imageVector = trailingIcon,
+                        contentDescription = null,
+                        tint = cursorColor,
+                        modifier = Modifier
+                            .clickable {
+                                if(onTrailingIconClick != null) onTrailingIconClick()
+                            }
+                    )
+                }
+            },
+            placeholder = {
+                Text(
+                    hint,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            },
+            textStyle = MaterialTheme.typography.bodyMedium,
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+            visualTransformation = visualTransformation
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextEntryModulePreview() {
+    TextEntryModule(
+        description = "Логин",
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp, 0.dp, 10.dp, 5.dp),
+        hint = "xd@xd.xd",
+        leadingIcon = Icons.Default.Email,
+        textValue = "",
+        textColor = Color.Gray,
+        cursorColor = Green,
+        onValueChanged = {},
+        trailingIcon = Icons.Default.RemoveRedEye,
+        onTrailingIconClick = { /*TODO*/ })
+}
