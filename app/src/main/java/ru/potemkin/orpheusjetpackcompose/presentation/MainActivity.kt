e2151fd25a74766f9e7855f3450cc464ce053004
SashaPotemkin <alex.potjomkin@gmail.com>
2023-09-21 14:21:02 +0300

    MainActivity

--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/MainActivity.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/MainActivity.kt
@@ -4,42 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.potemkin.orpheusjetpackcompose.presentation.components.ButtonComponent
+import ru.potemkin.orpheusjetpackcompose.presentation.navigation.MainNavigation
+import ru.potemkin.orpheusjetpackcompose.presentation.theme.OrpheusJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OrpheusJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainNavigation()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     OrpheusJetpackComposeTheme {
-        Greeting("penis")
+        MainNavigation()
     }
 }
