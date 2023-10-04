c7360a77fc2ce297a80c09a527dfcda720d156d1
SashaPotemkin <alex.potjomkin@gmail.com>
2023-10-04 11:44:37 +0300

    Отдельный компонент НавБара

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/components/NavigationBar.kt
@@ -0,0 +1,52 @@
+package ru.potemkin.orpheusjetpackcompose.presentation.components
+
+import android.util.Log
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChatBubble
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.Newspaper
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import ru.potemkin.orpheusjetpackcompose.data.BottomNavItem
+import ru.potemkin.orpheusjetpackcompose.presentation.navigation.CHAT_SCREEN
+import ru.potemkin.orpheusjetpackcompose.presentation.navigation.MAP_SCREEN
+import ru.potemkin.orpheusjetpackcompose.presentation.navigation.NEWS_SCREEN
+import ru.potemkin.orpheusjetpackcompose.presentation.navigation.PROFILE_SCREEN
+
+@Composable
+fun NavigationBar(navHostController: NavHostController) {
+
+    val items = listOf(
+        BottomNavItem("Новости", Icons.Default.Newspaper, NEWS_SCREEN),
+        BottomNavItem("Карта", Icons.Default.Map, MAP_SCREEN),
+        BottomNavItem("Чаты", Icons.Default.ChatBubble, CHAT_SCREEN),
+        BottomNavItem("Профиль", Icons.Default.Person, PROFILE_SCREEN)
+    )
+
+    BottomNavigation(
+        backgroundColor = Color.White
+    ) {
+        val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(imageVector = item.icon, contentDescription = item.label) },
+                label = { Text(text = item.label) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    Log.d("NAV", "Clicked ${item.route}")
+                    navHostController.navigate(item.route)
+                }
+            )
+        }
+    }
+
+}
--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/navigation/MainNavigation.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/navigation/MainNavigation.kt
@@ -7,8 +7,10 @@ import androidx.navigation.compose.rememberNavController
 import ru.potemkin.orpheusjetpackcompose.presentation.screens.ChatListScreen
 import ru.potemkin.orpheusjetpackcompose.presentation.screens.ChatScreen
 import ru.potemkin.orpheusjetpackcompose.presentation.screens.LoginScreen
+import ru.potemkin.orpheusjetpackcompose.presentation.screens.NewsFeedScreen
 import ru.potemkin.orpheusjetpackcompose.presentation.screens.RegistrationScreen
 import ru.potemkin.orpheusjetpackcompose.presentation.screens.StartScreen
+import ru.potemkin.orpheusjetpackcompose.presentation.screens.UserProfileScreen
 
 @Composable
 fun MainNavigation() {
@@ -31,6 +33,12 @@ fun MainNavigation() {
         composable(REG_SCREEN) {
             RegistrationScreen(navHostController)
         }
+        composable(NEWS_SCREEN) {
+            NewsFeedScreen(navHostController)
+        }
+        composable(PROFILE_SCREEN) {
+            UserProfileScreen(navHostController)
+        }
     }
 
 }
@@ -40,3 +48,6 @@ const val CHAT_LIST_SCREEN = "Chat List screen"
 const val CHAT_SCREEN = "Chat screen"
 const val LOG_SCREEN = "Login screen"
 const val REG_SCREEN = "Registration screen"
+const val NEWS_SCREEN = "News screen"
+const val MAP_SCREEN = "Map screen"
+const val PROFILE_SCREEN = "Profile screen"
