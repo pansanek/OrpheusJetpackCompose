d59c5d908f7ea551e5c447935a5552a7b7df43df
SashaPotemkin <alex.potjomkin@gmail.com>
2023-10-22 16:11:26 +0300

    Вьюмодели

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/viewmodels/ChatListViewModel.kt
@@ -0,0 +1,15 @@
+package ru.potemkin.orpheusjetpackcompose.presentation.viewmodels
+
+import androidx.lifecycle.ViewModel
+import ru.potemkin.orpheusjetpackcompose.data.UserRepositoryImpl
+import ru.potemkin.orpheusjetpackcompose.domain.usecases.user_usecases.GetUserListUseCase
+
+class ChatListViewModel : ViewModel() {
+
+    private val repository = UserRepositoryImpl
+
+    private val getUserListUseCase = GetUserListUseCase(repository)
+
+    val userList = getUserListUseCase.getUserList()
+
+}
---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/viewmodels/ChatViewModel.kt
@@ -0,0 +1,16 @@
+package ru.potemkin.orpheusjetpackcompose.presentation.viewmodels
+
+import androidx.lifecycle.ViewModel
+import ru.potemkin.orpheusjetpackcompose.data.MessageRepositoryImpl
+
+import ru.potemkin.orpheusjetpackcompose.domain.usecases.chat_usecases.GetMessageListUseCase
+
+class ChatViewModel: ViewModel() {
+
+    private val repository = MessageRepositoryImpl
+
+    private val getMessageListUseCase = GetMessageListUseCase(repository)
+
+    val messageList = getMessageListUseCase.getMessageList()
+
+}
