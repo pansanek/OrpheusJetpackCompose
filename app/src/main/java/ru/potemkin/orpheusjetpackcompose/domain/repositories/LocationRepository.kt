3cf151824ab835946056ea7da988fdac28158e09
pansanek <alex.potjomkin@gmail.com>
2023-10-25 18:20:31 +0300

    Репозиторий места

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/domain/repositories/LocationRepository.kt
@@ -0,0 +1,15 @@
+package ru.potemkin.orpheusjetpackcompose.domain.repositories
+
+import ru.potemkin.orpheusjetpackcompose.domain.entities.LocationItem
+
+interface LocationRepository {
+    fun addLocationItem(locationItem: LocationItem)
+
+    fun deleteLocationItem(locationItem: LocationItem)
+
+    fun editLocationItem(locationItem: LocationItem)
+
+    fun getLocationItem(locationId: Int): LocationItem
+
+    fun getLocationsList(): List<LocationItem>
+}
