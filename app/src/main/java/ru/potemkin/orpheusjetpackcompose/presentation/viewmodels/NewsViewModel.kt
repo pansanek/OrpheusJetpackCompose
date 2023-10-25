bdaf640f3e6d0aff8b498109d37139eaea80ce5b
pansanek <alex.potjomkin@gmail.com>
2023-10-25 19:11:07 +0300

    Вью модель страницы новостей

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/viewmodels/NewsViewModel.kt
@@ -0,0 +1,15 @@
+package ru.potemkin.orpheusjetpackcompose.presentation.viewmodels
+
+import androidx.lifecycle.ViewModel
+import ru.potemkin.orpheusjetpackcompose.data.PostRepositoryImpl
+import ru.potemkin.orpheusjetpackcompose.domain.usecases.post_usecases.GetPostListUseCase
+
+class NewsViewModel : ViewModel() {
+
+    private val repository = PostRepositoryImpl
+
+    private val getPostListUseCase = GetPostListUseCase(repository)
+
+    val postList = getPostListUseCase.getPostList()
+
+}
