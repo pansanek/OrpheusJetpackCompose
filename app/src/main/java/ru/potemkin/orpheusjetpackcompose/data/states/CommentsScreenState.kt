3c40863f1c48158ac8c05d53a8242fb6453939dd
SashaPotemkin <alex.potjomkin@gmail.com>
2023-11-13 16:49:53 +0300

    Попытка в стейт комментариев2(неудачная но потом доделать)

--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/data/states/CommentsScreenState.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/data/states/CommentsScreenState.kt
@@ -1,14 +1,14 @@
 package ru.potemkin.orpheusjetpackcompose.data.states
 
-import ru.potemkin.orpheusjetpackcompose.domain.entities.PostComment
-import ru.potemkin.orpheusjetpackcompose.domain.entities.PostItem
-
-sealed class CommentsScreenState {
-
-    object Initial : CommentsScreenState()
-
-    data class Comments(
-        val feedPost: PostItem,
-        val comments: List<PostComment>
-    ) : CommentsScreenState()
-}
+//import ru.potemkin.orpheusjetpackcompose.domain.entities.PostComment
+//import ru.potemkin.orpheusjetpackcompose.domain.entities.PostItem
+//
+//sealed class CommentsScreenState {
+//
+//    object Initial : CommentsScreenState()
+//
+//    data class Comments(
+//        val feedPost: PostItem,
+//        val comments: List<PostComment>
+//    ) : CommentsScreenState()
+//}
