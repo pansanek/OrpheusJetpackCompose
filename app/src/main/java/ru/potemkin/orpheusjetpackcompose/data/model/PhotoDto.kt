d9a5c544680cb741d4028d765dff8fc6a411e24c
pansanek <alex.potjomkin@gmail.com>
2023-11-27 18:53:26 +0300

    Network additions 2

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/data/model/PhotoDto.kt
@@ -0,0 +1,7 @@
+package ru.potemkin.orpheusjetpackcompose.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PhotoDto(
+    @SerializedName("sizes") val photoUrls: List<PhotoUrlDto>
+)
--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/viewmodels/AuthViewModel.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/viewmodels/AuthViewModel.kt
@@ -4,11 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.potemkin.orpheusjetpackcompose.data.mappers.AuthMapper
+import ru.potemkin.orpheusjetpackcompose.data.mappers.UsersMapper
 import ru.potemkin.orpheusjetpackcompose.data.network.ApiFactory
 import javax.inject.Inject
 
 class AuthViewModel @Inject constructor(): ViewModel() {
-    private val mapper = AuthMapper()
+    private val mapper = UsersMapper()
 
     init {
         loadAuthItems()
@@ -17,7 +18,7 @@ class AuthViewModel @Inject constructor(): ViewModel() {
     private fun loadAuthItems() {
         viewModelScope.launch{
             val users = ApiFactory.apiService.loadAllUsers()
-            val authItems = mapper.mapUserToAuth(users)
+            val authItems = mapper.mapUsers(users)
         }
     }
 
