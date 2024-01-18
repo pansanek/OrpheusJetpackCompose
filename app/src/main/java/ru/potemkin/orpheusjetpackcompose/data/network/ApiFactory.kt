5b92d12f2d858695ccc068ef2713c95329809c11
SashaPotemkin <alex.potjomkin@gmail.com>
2024-01-18 12:55:04 +0300

    Api Factory for services

--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/data/network/ApiFactory.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/data/network/ApiFactory.kt
@@ -6,6 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
+
 object ApiFactory {
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
@@ -13,11 +14,24 @@ object ApiFactory {
         })
         .build()
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:8081/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(okHttpClient)
-        .build()
+    // Замените "10.0.2.2" на IP-адрес вашего Docker-хоста, если вы используете эмулятор Android.
+    private val baseUrl = "http://10.0.2.2:"
 
-    val apiService: ApiService = retrofit.create()
+    val appUserApiService: ApiService = createService(baseUrl, 88)
+    val appPostApiService: ApiService = createService(baseUrl, 87)
+    val appMusicianApiService: ApiService = createService(baseUrl, 86)
+    val appLocationApiService: ApiService = createService(baseUrl, 85)
+    val appCommentApiService: ApiService = createService(baseUrl, 84)
+    val appBandApiService: ApiService = createService(baseUrl, 83)
+    val appAdministratorApiService: ApiService = createService(baseUrl, 80)
+
+    private fun createService(baseUrl: String, port: Int): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("$baseUrl$port/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
 }
