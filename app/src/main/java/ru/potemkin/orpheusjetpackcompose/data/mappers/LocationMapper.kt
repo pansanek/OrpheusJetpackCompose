407107538cb7165beea4a4217badfbd0923b4a08
SashaPotemkin <alex.potjomkin@gmail.com>
2024-01-18 12:50:50 +0300

    Location mapper

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/data/mappers/LocationMapper.kt
@@ -0,0 +1,43 @@
+package ru.potemkin.orpheusjetpackcompose.data.mappers
+
+import ru.potemkin.orpheusjetpackcompose.data.model.LocationDto
+import ru.potemkin.orpheusjetpackcompose.data.model.PhotoUrlDto
+import ru.potemkin.orpheusjetpackcompose.domain.entities.LocationItem
+import ru.potemkin.orpheusjetpackcompose.domain.entities.PhotoUrlItem
+
+class LocationMapper {
+
+    fun mapLocations(listLocationDto: List<LocationDto>): List<LocationItem> {
+        val result = mutableListOf<LocationItem>()
+        for (locationDto in listLocationDto) {
+            val locationItem = LocationItem(
+                id = locationDto.id,
+                adminId = locationDto.adminId,
+                name = locationDto.name,
+                address = locationDto.address,
+                about = locationDto.about,
+                profilePicture = mapPhotoUrlDtoToItem(locationDto.profilePicture)
+            )
+            result.add(locationItem)
+        }
+        return result
+    }
+
+    fun mapLocation(locationDto: LocationDto): LocationItem {
+        return LocationItem(
+            id = locationDto.id,
+            adminId = locationDto.adminId,
+            name = locationDto.name,
+            address = locationDto.address,
+            about = locationDto.about,
+            profilePicture = mapPhotoUrlDtoToItem(locationDto.profilePicture)
+        )
+    }
+
+    private fun mapPhotoUrlDtoToItem(photoUrlDto: PhotoUrlDto): PhotoUrlItem {
+        return PhotoUrlItem(
+            id = photoUrlDto.id,
+            url = photoUrlDto.url
+        )
+    }
+}
