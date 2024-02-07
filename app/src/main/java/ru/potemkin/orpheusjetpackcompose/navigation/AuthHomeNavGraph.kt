beb8f5f73fbc118b24c6c5bea9851e6675b503b2
SashaPotemkin <alex.potjomkin@gmail.com>
2024-02-07 15:14:53 +0300

    AuthHomeNavGraph

--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/navigation/AuthHomeNavGraph.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/navigation/AuthHomeNavGraph.kt
@@ -3,12 +3,21 @@ package ru.potemkin.orpheusjetpackcompose.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import ru.potemkin.orpheusjetpackcompose.domain.entities.AuthItems.AboutMeItem
+import ru.potemkin.orpheusjetpackcompose.domain.entities.AuthItems.RegItem
+import ru.potemkin.orpheusjetpackcompose.domain.entities.AuthItems.TypeItem
+import ru.potemkin.orpheusjetpackcompose.domain.entities.PostItem
 
 fun NavGraphBuilder.authHomeNavGraph(
     startScreenContent: @Composable () -> Unit,
     loginScreenContent: @Composable () -> Unit,
     registrationScreenContent: @Composable () -> Unit,
+    registrationAboutMeScreenContent: @Composable (RegItem) -> Unit,
+    registrationUserTypeScreenContent: @Composable (AboutMeItem) -> Unit,
+    registrationMusicianTypeScreenContent: @Composable (TypeItem) -> Unit,
+    registrationAdministratorTypeScreenContent: @Composable (TypeItem) -> Unit,
 ) {
     navigation(
         startDestination = Screen.StartScreen.route,
@@ -22,6 +31,54 @@ fun NavGraphBuilder.authHomeNavGraph(
         }
         composable(Screen.RegistrationScreen.route) {
             registrationScreenContent()
+        }
+        composable(
+            route = Screen.RegistrationAboutMeScreen.route,
+            arguments = listOf(
+                navArgument(Screen.KEY_REG_ABOUT_ME) {
+                    type = PostItem.NavigationType
+                }
+            )
+        ) {
+            val args = it.arguments?.getParcelable<RegItem>(Screen.KEY_REG_ABOUT_ME)
+                ?: throw RuntimeException("Args is null")
+            registrationAboutMeScreenContent(args)
+        }
+        composable(
+            route = Screen.RegistrationUserTypeScreen.route,
+            arguments = listOf(
+                navArgument(Screen.KEY_REG_TYPE) {
+                    type = PostItem.NavigationType
+                }
+            )
+        ) {
+            val args = it.arguments?.getParcelable<AboutMeItem>(Screen.KEY_REG_TYPE)
+                ?: throw RuntimeException("Args is null")
+            registrationUserTypeScreenContent(args)
+        }
+        composable(
+            route = Screen.RegistrationMusicianTypeScreen.route,
+            arguments = listOf(
+                navArgument(Screen.KEY_REG_MUS) {
+                    type = PostItem.NavigationType
+                }
+            )
+        ) {
+            val args = it.arguments?.getParcelable<TypeItem>(Screen.KEY_REG_MUS)
+                ?: throw RuntimeException("Args is null")
+            registrationMusicianTypeScreenContent(args)
+        }
+        composable(
+            route = Screen.RegistrationAdministratorTypeScreen.route,
+            arguments = listOf(
+                navArgument(Screen.KEY_REG_ADMN) {
+                    type = PostItem.NavigationType
+                }
+            )
+        ) {
+            val args = it.arguments?.getParcelable<TypeItem>(Screen.KEY_REG_ADMN)
+                ?: throw RuntimeException("Args is null")
+            registrationAdministratorTypeScreenContent(args)
         }
     }
 }
