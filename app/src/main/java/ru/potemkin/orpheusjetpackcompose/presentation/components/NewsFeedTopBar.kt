9840b8577ee074359e99799391427dd3e58a1afc
SashaPotemkin <alex.potjomkin@gmail.com>
2024-03-04 15:07:59 +0300

    TopBar с уведомлениями в новостях

---
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/components/NewsFeedTopBar.kt
@@ -0,0 +1,119 @@
+package ru.potemkin.orpheusjetpackcompose.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.potemkin.orpheusjetpackcompose.domain.entities.UserItem
+import ru.potemkin.orpheusjetpackcompose.presentation.components.my_user_profile_comp.MenuItem
+import ru.potemkin.orpheusjetpackcompose.ui.theme.Black
+import ru.potemkin.orpheusjetpackcompose.ui.theme.White
+
+@Composable
+fun NewsFeedTopBar(
+                        onDrawerClickListener: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+
+    ) {
+        TopAppBar(
+            title = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "Новости", color = White)
+                    Spacer(modifier = Modifier.weight(1f)) // Занимаем всю доступную ширину
+                    DrawerButton(onDrawerClickListener)
+                }
+            },
+            backgroundColor = Black
+        )
+
+
+    }
+}
+
+
+@Composable
+fun DrawerButton(onDrawerClickListener: () -> Unit) {
+    IconButton(
+        onClick = onDrawerClickListener,
+        modifier = Modifier.size(48.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Notifications,
+            contentDescription = "Уведомления",
+            tint = White
+        )
+    }
+}
+
+@Composable
+fun NewsFeedDrawerHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 32.dp, horizontal = 32.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Ваши уведомления", fontSize = 32.sp)
+    }
+}
+
+@Composable
+fun NewsFeedDrawerBody(
+    items: List<MenuItem>,
+    modifier: Modifier = Modifier,
+    itemTextStyle: TextStyle = TextStyle(fontSize = 18.sp),
+    onItemClick: (MenuItem) -> Unit
+) {
+    LazyColumn(modifier) {
+        items(items) { item ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        onItemClick(item)
+                    }
+                    .padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = item.icon,
+                    contentDescription = item.contentDescription,
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = item.title,
+                    style = itemTextStyle,
+                    modifier = Modifier.weight(1f),
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/newsfeed/news/NewsFeedScreen.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/newsfeed/news/NewsFeedScreen.kt
@@ -1,8 +1,14 @@
 package ru.potemkin.orpheusjetpackcompose.presentation.newsfeed.news
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Mic
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -11,7 +17,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 import ru.potemkin.orpheusjetpackcompose.domain.entities.PostItem
+import ru.potemkin.orpheusjetpackcompose.presentation.components.NewsFeedDrawerBody
+import ru.potemkin.orpheusjetpackcompose.presentation.components.NewsFeedDrawerHeader
+import ru.potemkin.orpheusjetpackcompose.presentation.components.NewsFeedTopBar
+import ru.potemkin.orpheusjetpackcompose.presentation.components.my_user_profile_comp.MenuItem
+import ru.potemkin.orpheusjetpackcompose.presentation.components.my_user_profile_comp.MyUserProfileTopBar
+import ru.potemkin.orpheusjetpackcompose.ui.theme.Black
 
 @Composable
 fun NewsFeedScreen(
@@ -20,13 +33,50 @@ fun NewsFeedScreen(
     val viewModel: NewsFeedViewModel = viewModel()
     val screenState = viewModel.screenState.observeAsState(NewsFeedScreenState.Initial)
 
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
     when (val currentState = screenState.value) {
         is NewsFeedScreenState.Posts -> {
-            FeedPosts(
-                paddingValues = paddingValues,
-                posts = currentState.posts,
-                onCommentClickListener = onCommentClickListener,
-                nextDataIsLoading = currentState.nextDataIsLoading
+            androidx.compose.material.Scaffold(
+                scaffoldState = scaffoldState,
+                topBar = {
+                    NewsFeedTopBar(onDrawerClickListener = {
+                        scope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    })
+                },
+                drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+                drawerContent = {
+                    NewsFeedDrawerHeader()
+                    NewsFeedDrawerBody(
+                        items = listOf(
+                            MenuItem(
+                                id = "help",
+                                title = "Help",
+                                contentDescription = "Get help",
+                                icon = Icons.Default.Info
+                            ),
+                        ),
+                        onItemClick = {
+
+                        }
+                    )
+                },
+                content = {
+                    Box(
+                        modifier = Modifier.fillMaxSize().background(Black),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        FeedPosts(
+                            topPaddingValues = it,
+                            bottomPaddingValues = paddingValues,
+                            posts = currentState.posts,
+                            onCommentClickListener = onCommentClickListener,
+                            nextDataIsLoading = currentState.nextDataIsLoading
+                        )
+                    }
+                }
             )
         }
 
@@ -41,17 +91,27 @@ fun NewsFeedScreen(
     }
 }
 
+
 @Composable
 private fun FeedPosts(
-    paddingValues: PaddingValues,
+    topPaddingValues: PaddingValues,
+    bottomPaddingValues: PaddingValues,
     posts: List<PostItem>,
     onCommentClickListener: (PostItem) -> Unit,
     nextDataIsLoading: Boolean
 ) {
     LazyColumn(
-        modifier = Modifier.padding(paddingValues), contentPadding = PaddingValues(
-            top = 16.dp, start = 8.dp, end = 8.dp, bottom = 16.dp
-        ), verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = Modifier.padding(
+            top = topPaddingValues.calculateTopPadding(),
+            bottom = bottomPaddingValues.calculateBottomPadding()
+        ),
+        contentPadding = PaddingValues(
+            top = 16.dp,
+            start = 8.dp,
+            end = 8.dp,
+            bottom = 16.dp
+        ),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(items = posts, key = { it.id }) { feedPost ->
             PostItem(
