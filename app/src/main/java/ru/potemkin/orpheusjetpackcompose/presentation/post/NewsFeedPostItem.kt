ed8ecc7bcb91c491d4a66f38ce5f5805fcb82da2
SashaPotemkin <alex.potjomkin@gmail.com>
2024-03-13 13:06:15 +0300

    Дропдаун меню постов

--- app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/post/NewsFeedPostItem.kt
+++ app/src/main/java/ru/potemkin/orpheusjetpackcompose/presentation/post/NewsFeedPostItem.kt
@@ -2,6 +2,7 @@ package ru.potemkin.orpheusjetpackcompose.presentation.post
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,14 +12,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -102,6 +113,7 @@ private fun NewsFeedPostHeader(
     onUserClickListener: (UserItem) -> Unit,
     onBandClickListener: (BandItem) -> Unit
 ) {
+    var showMenu by remember { mutableStateOf(false) }
     Row(
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
@@ -135,11 +147,53 @@ private fun NewsFeedPostHeader(
                 color = White
             )
         }
-        Icon(
-            imageVector = Icons.Rounded.MoreVert,
-            contentDescription = null,
-            tint = White
-        )
+        Box(
+            modifier = Modifier
+                .wrapContentSize(Alignment.TopEnd)
+        ) {
+            IconButton(
+                onClick = {
+                    showMenu = !showMenu
+                },
+                modifier = Modifier.size(48.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.MoreVert,
+                    contentDescription = null,
+                    tint = White,
+
+                    )
+            }
+            DropdownMenu(
+                containerColor = Black,
+                shadowElevation = 16.dp,
+                tonalElevation = 16.dp,
+                expanded = showMenu,
+                onDismissRequest = { showMenu = false }) {
+                DropdownMenuItem(
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Rounded.Edit,
+                            contentDescription = null,
+                            tint = White
+                        )
+                    },
+                    text = { Text(text = "Редактировать",color = White) },
+                    onClick = { }
+                )
+                DropdownMenuItem(
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Rounded.Warning,
+                            contentDescription = null,
+                            tint = Red
+                        )
+                    },
+                    text = { Text(text = "Пожаловаться",color = Red) },
+                    onClick = { }
+                )
+            }
+        }
     }
 }
 
